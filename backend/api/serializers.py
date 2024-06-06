8b6727652331a83a4b1a8041d6e2bd7144b1b53c
Marsallis <marsallisf@gmail.com>
2024-06-06 13:38:55 -0400

    finished backend
    added models, finished urls and views, and fixed serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Note
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,7 +8,14 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "username", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
-        #method that will be called when we want to create a new version of a user
-        def create(self, validated_data):
-            user = User.objects.create_user(**validated_data)
-            return user
+    #method that will be called when we want to create a new version of a user
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+        
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
+
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,8 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication"
     ],
 }
 
