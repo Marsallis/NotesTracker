e851de110b689a5a8526cda000e55acd2b32a60a
Marsallis <marsallisf@gmail.com>
2024-06-07 09:05:33 -0400

    update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
-const apiUrl = "/choreo-apis/notes-qh/backend/v1"
-
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+    baseURL: import.meta.env.VITE_API_URL,
   });
+  
+  console.log("Base URL:", api.defaults.baseURL);
 
 api.interceptors.request.use(
     (config) => {
