ddee4c11bced7627f26ba1681641b96e1f7c64a3
Marsallis <marsallisf@gmail.com>
2024-06-06 22:23:21 -0400

    small changes to backend settings

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Note(models.Model):
+class Note(models.Model): 
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
